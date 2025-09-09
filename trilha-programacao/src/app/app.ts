189f85869d0dc8a01c107e8c0005d6638a14c735
David Dias <david.nascimento@essentia.com.br>
2025-09-09 12:05:13 -0300

    Adicionando cards

--- trilha-programacao/src/app/app.ts
+++ trilha-programacao/src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Navbar } from './src/app/shared/navbar/navbar';
 import { Footer } from './src/app/shared/footer/footer';
 import { LandingPage } from './src/app/components/landing-page/landing-page';
+import { Roads } from './src/app/components/roads/roads';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, Footer, LandingPage],
+  imports: [Navbar, Footer, LandingPage, Roads],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
