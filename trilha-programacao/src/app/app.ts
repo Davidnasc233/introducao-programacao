218739dcbe2d772d2ba577feab38656e1f823b01
David.nasc233 <david.nasc233@gmail.com>
2025-09-10 00:04:48 -0300

    Fazendo estilo do componente cards

--- trilha-programacao/src/app/app.ts
+++ trilha-programacao/src/app/app.ts
@@ -2,12 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './src/app/shared/navbar/navbar';
 import { Footer } from './src/app/shared/footer/footer';
-import { LandingPage } from './src/app/components/landing-page/landing-page';
 import { Roads } from './src/app/components/roads/roads';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, Footer, LandingPage, Roads],
+  imports: [Navbar, Footer, Roads],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
--- trilha-programacao/src/app/src/app/components/roads/roads.ts
+++ trilha-programacao/src/app/src/app/components/roads/roads.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-roads',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './roads.html',
-  styleUrl: './roads.css'
+  styleUrl: './roads.css',
 })
 export class Roads {
-
+  @Input() imageSrc: string = ''; // Caminho da imagem
+  @Input() title: string = ''; // Título do card
+  @Input() description: string = ''; // Descrição do card
+  @Input() link: string = ''; // Link do botão
 }
